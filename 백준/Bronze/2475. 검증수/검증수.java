25cd876504c881ba6ca351e86a69f03144929471
Sumin <113510973+infiniment@users.noreply.github.com>
2025-03-15 14:15:15 +0900

    [Bronze V] Title: 검증수, Time: 168 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	    int[] arr = new int[5];
+	    int sum = 0;
+	   
+	    
+	    for(int i = 0; i < arr.length; i++) {
+	        arr[i] = sc.nextInt();
+	        sum += arr[i] * arr[i];
+	    }
+	    
+	    System.out.println(sum % 10);
+	    
+	}
+}
