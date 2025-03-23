944b371c4c3b311ddf65aa7c57e0de353c411392
Sumin <113510973+infiniment@users.noreply.github.com>
2025-03-23 18:13:45 +0900

    [Bronze I] Title: 평균, Time: 296 ms, Memory: 25004 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        double[] scores = new double[n];
+        double max = 0;
+        
+ 
+        for (int i = 0; i < n; i++) {
+            scores[i] = sc.nextDouble();
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+        }
+
+
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (scores[i] / max) * 100;
+        }
+
+
+        double result = sum / n;
+        System.out.println(result);
+    }
+}
